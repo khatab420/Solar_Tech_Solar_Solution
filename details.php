15470e2fe1e888aee859f91b789c4216157815d2
khanwardak <36572475+khanwardak@users.noreply.github.com>
2022-08-23 02:54:05 -0700

    Add files via upload
    product details page created

---
+++ details.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Solar System</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+
+    <!-- Favicon -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="style.css?verssion=3" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid">
+       <!--  top nawbar strat here ----------------------------------------------------------------------------------------------------------------------------------------------->
+ <div class="row  topnav">
+                <div class="col-lg-6 text-start text-lg-right language">
+                <div class="d-inline-flex align-items-center">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">دری</button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <button class="dropdown-item" type="button">پښتو</button>
+                            <button class="dropdown-item" type="button">دری</button>
+                        </div>
+                    </div>
+                  
+                </div>
+                <div class="d-inline-flex align-items-center d-block d-lg-none">
+                    <a href="" class="btn px-0 ml-2">
+                        <i class="fas fa-heart text-dark"></i>
+                        <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
+                    </a>
+                    <a href="" class="btn px-0 ml-2">
+                        <i class="fas fa-shopping-cart text-dark"></i>
+                        <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block text-end topnave-right " >
+                <div class="d-inline-flex align-items-left ">
+                   
+                    <a class="text-body mr-3" href=""> اړیکه  </a>
+                    <a class="text-body mr-3 text-direction-none" href=""> رموږ په اړه   </a>
+                   
+                </div>
+            </div>
+            </div>
+          <div class="row align-items-start bg-light py-3 px-xl-5 d-none d-lg-flex " style="direction: rtl;">
+            <div class="col-lg-4 d-flex justify-content-start ">
+                <a href="" class="text-decoration-none">
+                    <span class="h1 text-uppercase text-warning bg-dark px-2">SOLAR</span>
+                    <span class="h1 text-uppercase text-dark bg-warning px-2 ml-n1">SYSTEM</span>
+                </a>
+            </div>
+            <div class="col-lg-4 col-6 text-right">
+                <form action="">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="">
+                        <div class="input-group-append ">
+                            <span class=" form-control bg-transparent text-warning">
+                                <i class="fa fa-search "></i>
+                            </span>
+                        </div>
+                    </div>
+                    </form>
+            </div>
+            <div class="col-lg-4 col-6 text-left  d-flex justify-content-end">
+                <p class="m-8" style="margin-left: 7px;">د مشتریانو خدمتونه</p>
+                  <h5 class="m-0">+93 7777884884</h5>
+                
+              
+            </div>
+        </div>
+
+
+        <!-- Navbar Start  here--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+    <div class="container-fluid bg-dark mb-30" style="direction: rtl;">
+        <div class="row px-xl-5">
+            <div class="col-lg-3 d-none d-lg-block">
+                <a class="btn d-flex align-items-center justify-content-between bg-warning w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
+                    <h6 class="text-dark m-0"><i class="fa fa-bars mr-2 text-align-right"></i>کتګوری</h6>
+                    <i class="fa fa-angle-down text-dark"></i>
+                </a>
+                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(28% - 30px); z-index: 999;">
+                    <div class="navbar-nav w-100" style="text-align: right;">
+                        <div class="nav-item dropdown dropright">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Buttery <i class="fa fa-angle-right float-right mt-1"></i></a>
+                            <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <a href="" class="dropdown-item">1</a>
+                                <a href="" class="dropdown-item">1</a>
+                                <a href="" class="dropdown-item">2</a>
+                            </div>
+                        </div>
+                        <a href="" class="nav-item nav-link">2</a>
+                        <a href="" class="nav-item nav-link">2</a>
+                        <a href="" class="nav-item nav-link">2</a>
+                        <a href="" class="nav-item nav-link">3</a>
+                        <a href="" class="nav-item nav-link">4</a>
+                        <a href="" class="nav-item nav-link">5</a>
+                        <a href="" class="nav-item nav-link">6</a>
+                        <a href="" class="nav-item nav-link">7</a>
+                        <a href="" class="nav-item nav-link">8</a>
+                    </div>
+                </nav>
+            </div>
+             <div class="col-lg-9">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
+                    <a href="" class="text-decoration-none d-block d-lg-none">
+                        <span class="h1 text-uppercase text-dark bg-light px-2">SOLAR</span>
+                        <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">SYSTEM</span>
+                    </a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="index.html" class="nav-item nav-link active">کور پاڼه</a>
+                            <a href="shop.html" class="nav-item nav-link">سټور </a>
+                            <a href="detail.html" class="nav-item nav-link">ګودام</a>
+                         
+                            <a href="contact.html" class="nav-item nav-link"> زموږ سره اړیکه </a>
+                        </div>
+                        <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
+                            <a href="" class="btn px-0">
+                                <i class="fas fa-heart text-primary"></i>
+                                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+                            </a>
+                            <a href="" class="btn px-0 ml-3">
+                                <i class="fas fa-shopping-cart text-primary"></i>
+                                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Navbar End here ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+
+    <!-- Breadcrumb Start -->
+    <div class="container-fluid" style="direction:rtl">
+        <div class="row px-xl-5">
+            <div class="col-12" style="direction: rtl;">
+                <nav class="breadcrumb bg-light mb-30">
+                    <a class="breadcrumb-item text-dark text-decoration-none" href="#">کور پاڼه</a>
+                   <span class="breadcrumb-item active"></span>
+                    <span class="breadcrumb-item active">جزیات</span>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+
+     <!-- Shop Detail Start------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+    <div class="container-fluid pb-5" style="direction:rtl">
+        <div class="row px-xl-5">
+            <div class="col-lg-5 mb-30">
+                <div id="product-carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner bg-light">
+                        <div class="carousel-item active">
+                            <img class="w-100 h-100" src="img/butery2.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="img/butter3.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="img/butery6.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="img/sumersible.jpg" alt="Image">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
+                        <i class="fa fa-2x fa-angle-left text-dark"></i>
+                    </a>
+                    <a class="carousel-control-next" href="#product-carousel" data-slide="next">
+                        <i class="fa fa-2x fa-angle-right text-dark"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-7 h-auto mb-30">
+                <div class="h-100 bg-light p-30">
+                    <h3 style="text-align:right;">د محصول نوم</h3>
+                    <div class="d-flex mb-3">
+                       <!--  <div class="text-primary mr-2">
+                            <small class="fas fa-star"></small>
+                            <small class="fas fa-star"></small>
+                            <small class="fas fa-star"></small>
+                            <small class="fas fa-star-half-alt"></small>
+                            <small class="far fa-star"></small>
+                        </div> -->
+                      <!--   <small class="pt-1">(99 کتل شوی)</small> -->
+                    </div>
+                    <h3 style="text-align: right;" class="font-weight-semi-bold mb-4">اف150.00</h3>
+                    <p style="text-align: right;" class="mb-4">د محصول په اړه معلدمات</p>
+                    <div class="d-flex mb-3">
+                        <strong class="text-dark mr-3">سایز:</strong>
+                        
+                    </div>
+                    <div class="d-flex mb-4">
+                        <strong class="text-dark mr-3">رنګ:</strong>
+                       
+                    </div>
+                  
+                    <div class="d-flex pt-2">
+                        <strong class="text-dark mr-2">شریک یی کړی:</strong>
+                        <div class="d-inline-flex">
+                            <a class="text-dark px-2" href="">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a class="text-dark px-2" href="">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a class="text-dark px-2" href="">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                            <a class="text-dark px-2" href="">
+                                <i class="fab fa-pinterest"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="row px-xl-5">
+            <div class="col">
+                <div class="bg-light p-30">
+                    <div class="nav nav-tabs mb-4">
+                        <a class="nav-item nav-link text-dark active" data-toggle="tab" href="#tab-pane-1">جریات</a>
+                      
+                        <a class="nav-item nav-link text-dark" data-toggle="tab" href="#tab-pane-3">نظر.نه(0)</a>
+                    </div>
+                    <div class="tab-content" style="direction:rtl;">
+                        <div class="tab-pane fade show active" id="tab-pane-1" style="text-align:right;">
+                            <h4 class="mb-3">د محصول په اړه معلومات</h4>
+                            <p>د محصول په اړه معلومات د محصول په اړه معلومات د محصول په اړه معلومات د محصول په اړه معلومات د محصول په اړه معلومات د محصول په اړه معلومات د محصول په اړه معلومات د محصول په اړه معلومات.</p>
+                            <p>د محصول په اړه معلومات.</p>
+                        </div>
+                      
+                        <div class="tab-pane fade" id="tab-pane-3">
+                            <div class="row">
+                                  <div class="col-md-6" style="text-align: right;">
+                                    <h4 class="mb-4">نظر مو پریږدی</h4>
+                                    <small>ستاسو ایمل ادرس به نه شریک کیږی. په نښه شوی فیلډونه ضروری دی *</small>
+                                    <div class="d-flex my-3">
+                                      
+                                    </div>
+                                      <form>
+                                        <div class="form-group">
+                                            <label for="message">نطر*</label>
+                                            <textarea id="message" cols="30" rows="5" class="form-control"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="name">نوم *</label>
+                                            <input type="text" class="form-control" id="name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email">ایمل *</label>
+                                            <input type="email" class="form-control" id="email">
+                                        </div>
+                                        <div class="form-group mb-0">
+                                            <input type="submit" value="شریک یی کری" class="btn btn-primary px-3">
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-6">
+                                    <h4 class="mb-4">1 د مشتریانو نطرونه د مخصول په اړه"</h4>
+                                    <div class="media mb-4">
+                                        <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px;">
+                                        <div class="media-body">
+                                            <h6>نوم<small> - <i>تاریخ</i></small></h6>
+                                          
+                                            <p>دا بظری ما اخستې وه یوه افغانی هم ارزښت نه لری.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                              
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Shope Detail End -->
+</div>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    
+    <script src="js/main.js"></script>
+</body>
+</html>
